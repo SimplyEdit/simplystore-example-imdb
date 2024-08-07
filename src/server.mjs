42c3a03a8049dde4605c59885a0c1983d20ac3fd
Auke van Slooten <auke@muze.nl>
2024-08-07 17:26:30 +0200

    fixed loading dataset

--- src/server.mjs
+++ src/server.mjs
@@ -1,7 +1,10 @@
 import simplystore from '@muze-nl/simplystore'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url';
 
-const root = dirname(process.cwd())
+const root = path.dirname(path.dirname(fileURLToPath(import.meta.url)))
 
+console.log('Loading data from '+root+'/data/data.od-jsontag')
 simplystore.get('/', (req, res) => {
   res.sendFile(root + '/www/index.html')
 })
