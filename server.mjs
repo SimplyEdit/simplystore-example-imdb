b8ff7989427a41c34bac45d118742bca107bf1a8
Ben Peachey <ben@muze.nl>
2024-08-07 15:13:07 +0200

    Add index page.

--- server.mjs
+++ server.mjs
@@ -1,5 +1,9 @@
 import simplystore from '@muze-nl/simplystore'
 
+simplystore.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/index.html')
+})
+
 simplystore.run({
   datafile: process.cwd() + '/data.od-jsontag'
 })
